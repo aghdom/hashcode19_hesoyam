fca5af68916d4a360e9c7406d9e0f842095e42fb
Dominik Ágh <agh.dominik@gmail.com>
2019-02-28 19:15:13 +0100

    Data and input load

--- main.py
+++ main.py
@@ -1,63 +1,72 @@
-import random
-from deap import tools, base, creator
-
-IND_SIZE = 10
-
-toolbox = base.Toolbox()
-toolbox.register("attribute", random.random)
-toolbox.register("individual", tools.initRepeat, creator.Individual,
-                 toolbox.attribute, n=IND_SIZE)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-
-def evaluate(individual):
-    return sum(individual),
-
-
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("evaluate", evaluate)
-
-
-def main():
-    pop = toolbox.population(n=50)
-    cxpb, mutpb, ngen = 0.5, 0.2, 40
-
-    # Evaluate the entire population
-    fitnesses = map(toolbox.evaluate, pop)
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
-
-    for g in range(ngen):
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop))
-        # Clone the selected individuals
-        offspring = map(toolbox.clone, offspring)
-
-        # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() < cxpb:
-                toolbox.mate(child1, child2)
-                del child1.fitness.values
-                del child2.fitness.values
-
-        for mutant in offspring:
-            if random.random() < mutpb:
-                toolbox.mutate(mutant)
-                del mutant.fitness.values
-
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
-
-        # The population is entirely replaced by the offspring
-        pop[:] = offspring
-
-    return pop
+# import random
+# from deap import tools, base, creator
+#
+# IND_SIZE = 10
+#
+# toolbox = base.Toolbox()
+# toolbox.register("attribute", random.random)
+# # toolbox.register("individual", tools.initRepeat, creator.Individual,
+#                  toolbox.attribute, n=IND_SIZE)
+# # toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+#
+#
+# def evaluate(individual):
+#     return sum(individual),
+#
+#
+# toolbox.register("mate", tools.cxTwoPoint)
+# toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+# toolbox.register("select", tools.selTournament, tournsize=3)
+# toolbox.register("evaluate", evaluate)
+#
+#
+# def main():
+#     # pop = toolbox.population(n=50)
+#     cxpb, mutpb, ngen = 0.5, 0.2, 40
+#
+#     # Evaluate the entire population
+#     # fitnesses = map(toolbox.evaluate, pop)
+#     for ind, fit in zip(pop, fitnesses):
+#         ind.fitness.values = fit
+#
+#     for g in range(ngen):
+#         # Select the next generation individuals
+#         # offspring = toolbox.select(pop, len(pop))
+#         # Clone the selected individuals
+#         # offspring = map(toolbox.clone, offspring)
+#
+#         # Apply crossover and mutation on the offspring
+#         for child1, child2 in zip(offspring[::2], offspring[1::2]):
+#             if random.random() < cxpb:
+#                 # toolbox.mate(child1, child2)
+#                 del child1.fitness.values
+#                 del child2.fitness.values
+#
+#         for mutant in offspring:
+#             if random.random() < mutpb:
+#                 toolbox.mutate(mutant)
+#                 del mutant.fitness.values
+#
+#         # Evaluate the individuals with an invalid fitness
+#         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+#         fitnesses = map(toolbox.evaluate, invalid_ind)
+#         for ind, fit in zip(invalid_ind, fitnesses):
+#             ind.fitness.values = fit
+#
+#         # The population is entirely replaced by the offspring
+#         pop[:] = offspring
+#
+#     return pop
 
 
 if __name__ == '__main__':
-    main()
+    pictures = []
+    with open('data/a_example.txt') as f:
+        f.readline()
+        for line in f.readlines():
+            parsed_line = line.split(' ')
+            pictures.append({'type': parsed_line[0], 'tags': parsed_line[2:]})
+            pictures[-1]['tags'][-1] = pictures[-1]['tags'][-1].strip('\n')
+    for pic in pictures:
+        print(pic)
+    # main()
